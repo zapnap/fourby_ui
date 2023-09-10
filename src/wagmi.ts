bcfb5059e428780fd36f09f42807c31c9d3b39d9
Nick Plante <nap@zerosum.org>
2023-09-10 16:39:49 -0400

    remove walletconnect for now - needs config

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -4,7 +4,7 @@ import { foundry, goerli, mainnet } from 'wagmi/chains'
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+// import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
 import { publicProvider } from 'wagmi/providers/public'
 
@@ -18,11 +18,30 @@ export const { chains, publicClient, webSocketPublicClient } = configureChains(
   ],
 )
 
+const connectors = [
+  new MetaMaskConnector({ chains }),
+  new CoinbaseWalletConnector({
+    chains,
+    options: {
+      appName: 'wagmi',
+    },
+  }),
+  new InjectedConnector({
+    chains,
+    options: {
+      name: 'Injected',
+      shimDisconnect: true,
+    },
+  }),
+]
+
+/*
 const { connectors } = getDefaultWallets({
   appName: 'Fourby',
-  projectId: String(process.env.WALLETCONNECT_PROJECT_ID),
+  projectId: '', //String(process.env.WALLETCONNECT_PROJECT_ID),
   chains
 })
+*/
 
 export const config = createConfig({
   autoConnect: true,
